98fe6d207a0c45d068d07cb1e2ee0eeef9bf28ea
manjeet kumar <kumarmanjeet50728@gmail.com>
2026-05-30 22:35:39 +0530

    Added contest APIs

--- server/routes/contestRoutes.js
+++ server/routes/contestRoutes.js
@@ -21,6 +21,35 @@ router.get("/", async (req, res) => {
     }
 })
 
+
+router.get("/:id", async (req, res) => {
+    try{
+        const {id} = req.params;
+
+        const contest = await prisma.contest.findUnique({
+            where: {
+                id : Number(id),
+            },
+        });
+
+        if(!contest){
+            return res.status(404).json({
+                message: "Contest not found",
+            });
+        }
+
+        return res.status(200).json({
+             contest
+        })
+    }catch(err){
+        console.log(err);
+
+        return res.status(500).json({
+            message: "Server Error",
+        })
+    }
+});
+
 router.post("/create", authMiddleware, async(req, res) => {
     try{
         const {
@@ -29,6 +58,19 @@ router.post("/create", authMiddleware, async(req, res) => {
             startTime,
             endTime
         } = req.body;
+
+    if(!title || !description || !startTime || !endTime){
+    return res.status(400).json({
+        message: "All field are required"
+    })
+    }
+
+     if(new Date(endTime) <= new Date(startTime)){
+    res.status(400).json({
+        message : "End time must be after start time"
+    })
+     }
+
 
         const contest = await prisma.contest.create({
             data: {
