12f82a8557982c709f0d8d3c83faf74546e45c57
manjeet kumar <kumarmanjeet50728@gmail.com>
2026-06-02 00:15:06 +0530

    Setup frontend and fetch contests

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Contests from "./pages/Contests";
+
+
+function App() {
+  return (
+    <div>
+      <h1>ContestHub</h1>
+      <p>Frontend setup completed 🚀</p>
+      <Contests/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Contests.jsx
@@ -0,0 +1,33 @@
+import  { useEffect , useState} from "react"
+import api from "../services/api"
+import axios from "axios";
+
+function Contests() {
+    const [contest, setContest] = useState([]);
+
+    useEffect(() => {
+        const fetchContests = async () => {
+            try {
+                const res = await api.get("/contest");
+                console.log(res.data);
+                setContest(res.data);
+            }catch(err){
+                console.log(err)
+            }
+        };
+        fetchContests();
+    }, []);
+  return (
+    <div>
+      <h1>All Contests</h1>
+      {contest.map((contest) => (
+        <div key={contest.id}>
+            <h2>{contest.title}</h2>
+            <p>{contest.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Contests;
