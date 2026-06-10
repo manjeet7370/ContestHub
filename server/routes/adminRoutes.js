954e3296b47307c51b5ae94acf605d1dbd28a0ef
manjeet kumar <kumarmanjeet50728@gmail.com>
2026-06-10 19:13:56 +0530

    Add RBAC and admin contest/problem management

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const prisma = require("../prisma/prismaClient");
 
 const authMiddleware = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
@@ -9,6 +10,75 @@ router.get("/dashboard", authMiddleware, adminMiddleware, (req , res) => {
     res.json({
         message: "Admin Dashboard",
     })
+})
+
+router.post(
+    "/contest/create", authMiddleware, adminMiddleware,
+   async (req, res) => {
+     try{
+        const {
+            title,
+            description,
+            startTime,
+            endTime,
+        } = req.body;
+
+        const contest = await prisma.contest.create({
+            data: {
+                title,
+                description,
+                startTime: new Date(startTime),
+                endTime: new Date(endTime),
+                createdById: req.user.id,
+            },
+        });
+
+        res.status(201).json({
+            message: "Contest create successfully",
+            contest,
+        });
+     }catch(err){
+        console.log(err)
+        
+        res.status(500).json({
+            message: "Server Error",
+        });
+     }
+
+    }
+)
+
+
+router.post("/problem/create", authMiddleware, adminMiddleware, async(req, res) => {
+    try{
+        const {
+           title,
+           statement,
+           difficulty,
+           contestId,
+        }  = req.body;
+
+        const problem  = await prisma.problem.create({
+            data: {
+                title,
+                statement,
+                difficulty,
+                contestId,
+            },
+        });
+
+        res.status(201).json({
+            message: "Problem created successfully",
+            problem,
+        });
+
+    }catch(err){
+        console.log(err);
+
+        res.status(500).json({
+            message: "Server Error",
+        });
+    }
 })
 
 
